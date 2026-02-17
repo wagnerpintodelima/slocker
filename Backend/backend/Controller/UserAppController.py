3d2fb1c5e21e71daa58e07099411e6ac5f55aa31
Wagner <wagnerdelima2@gmail.com>
2026-02-17 14:47:34 -0300

    Ajuste no cadastro do User App

--- Backend/backend/Controller/UserAppController.py
+++ Backend/backend/Controller/UserAppController.py
@@ -67,54 +67,56 @@ def editAction(request, user_app_id):
         address = UserAppAddress.objects.get(id=item.user_app_address.id)
 
     if request.method == 'POST' and item:
+        
         nome = request.POST.get('name', None)
-        signature = request.POST.get('signature', None)
         password = request.POST.get('password', None)
         hu = request.POST.get('hu', None)
-        email = request.POST.get('email', None)
         cpf = request.POST.get('cpf', None)
-        telefone = request.POST.get('telefone', None)
-        cep = request.POST.get('cep', None)
-        uf = request.POST.get('uf', None)
-        cidade = request.POST.get('cidade', None)
-        bairro = request.POST.get('bairro', None)
-        rua = request.POST.get('rua', None)
-        numero = request.POST.get('numero', None)
-        observacao = request.POST.get('observacao', None)
-        foto = None
-        if 'foto' in request.FILES:
-            foto = request.FILES['foto']
         status = request.POST.get('status', 0)
+        
+        # email = request.POST.get('email', None)
+        # signature = request.POST.get('signature', None)
+        # telefone = request.POST.get('telefone', None)
+        # cep = request.POST.get('cep', None)
+        # uf = request.POST.get('uf', None)
+        # cidade = request.POST.get('cidade', None)
+        # bairro = request.POST.get('bairro', None)
+        # rua = request.POST.get('rua', None)
+        # numero = request.POST.get('numero', None)
+        # observacao = request.POST.get('observacao', None)
+        # foto = None
+        # if 'foto' in request.FILES:
+        #     foto = request.FILES['foto']
 
-        if item.user_app_address:
-            address.cep = cep
-            address.uf_description = uf
-            address.city = cidade
-            address.neighborhood = bairro
-            address.street = rua
-            address.house_number = numero
-            address.observation = observacao
-            address.status = status
-            address.updated_by = request.user.id
-            address.updated_at = datetime.datetime.now()
-            address.save()
+        # if item.user_app_address:
+        #     address.cep = cep
+        #     address.uf_description = uf
+        #     address.city = cidade
+        #     address.neighborhood = bairro
+        #     address.street = rua
+        #     address.house_number = numero
+        #     address.observation = observacao
+        #     address.status = status
+        #     address.updated_by = request.user.id
+        #     address.updated_at = datetime.datetime.now()
+        #     address.save()
 
         item.name = nome
         item.hu = hu
         item.password = password
-        item.signature = signature
-        item.email = email
+        # item.signature = signature
+        # item.email = email
         item.cpf = cpf
-        item.phone_number = telefone
+        # item.phone_number = telefone
         item.updated_at = datetime.datetime.now()
         item.updated_by = request.user.id
-        if foto:
-            try:
-                os.remove('media/' + _PATH_FILE_USER_APP + item.picture + '.png')
-            except Exception as er:
-                pass
+        # if foto:
+        #     try:
+        #         os.remove('media/' + _PATH_FILE_USER_APP + item.picture + '.png')
+        #     except Exception as er:
+        #         pass
 
-            item.picture = saveFile(_PATH_FILE_USER_APP, 'png', foto)
+        #     item.picture = saveFile(_PATH_FILE_USER_APP, 'png', foto)
 
         item.status = status
         item.save()
@@ -127,53 +129,56 @@ def editAction(request, user_app_id):
 def saveAction(request):
 
     if request.method == 'POST':
-        nome = request.POST.get('name', None)
+        
         client_id = request.POST.get('client_id', None)
         client = Client.objects.get(id=client_id)
+        nome = request.POST.get('name', None)
         hu = request.POST.get('hu', None)
         password = request.POST.get('password', None)
-        signature = request.POST.get('signature', None)
-        email = request.POST.get('email', None)
         cpf = request.POST.get('cpf', None)
-        telefone = request.POST.get('telefone', None)
-        cep = request.POST.get('cep', None)
-        uf = request.POST.get('uf', None)
-        bairro = request.POST.get('bairro', None)
-        rua = request.POST.get('rua', None)
-        numero = request.POST.get('numero', None)
-        cidade = request.POST.get('cidade', None)
-        observacao = request.POST.get('observacao', None)
-        foto = request.FILES['foto']
         status = request.POST.get('status', 0)
+        
+        # signature = request.POST.get('signature', None)
+        # email = request.POST.get('email', None)
+        # telefone = request.POST.get('telefone', None)
+        # cep = request.POST.get('cep', None)
+        # uf = request.POST.get('uf', None)
+        # bairro = request.POST.get('bairro', None)
+        # rua = request.POST.get('rua', None)
+        # numero = request.POST.get('numero', None)
+        # cidade = request.POST.get('cidade', None)
+        # observacao = request.POST.get('observacao', None)
+        # foto = request.FILES['foto']
+        
 
-        address = UserAppAddress()        
-        address.title = 'UsuárioApp'
-        address.cep = cep
-        address.uf_description = uf
-        address.city = cidade
-        address.neighborhood = bairro
-        address.street = rua
-        address.house_number = numero
-        address.observation = observacao
-        address.status = status
-        address.created_by = request.user.id
-        address.created_at = datetime.datetime.now()
-        address.save()
+        # address = UserAppAddress()        
+        # address.title = 'UsuárioApp'
+        # address.cep = cep
+        # address.uf_description = uf
+        # address.city = cidade
+        # address.neighborhood = bairro
+        # address.street = rua
+        # address.house_number = numero
+        # address.observation = observacao
+        # address.status = status
+        # address.created_by = request.user.id
+        # address.created_at = datetime.datetime.now()
+        # address.save()
 
         item = UserApp()
         item.client = client
         item.hu = hu
-        item.user_app_address = address
+        # item.user_app_address = address
         item.name = nome
         item.password = password
-        item.signature = signature
-        item.email = email
+        # item.signature = signature
+        # item.email = email
         item.cpf = cpf
-        item.phone_number = telefone
+        # item.phone_number = telefone
         item.created_at = datetime.datetime.now()
         item.created_by = request.user.id
-        if foto:
-            item.picture = saveFile(_PATH_FILE_USER_APP, 'png', foto)
+        # if foto:
+        #     item.picture = saveFile(_PATH_FILE_USER_APP, 'png', foto)
         item.status = status
         item.save()
 
@@ -191,10 +196,15 @@ def deleteAction(request, user_app_id):
     except Exception as er:
         messages.add_message(request, messages.ERROR, "Ocorreu esse erro ao tentar deletar a foto: {}".format(er))
 
-    try:
-        address = UserAppAddress.objects.get(id=item.user_app_address.id)
-        item.delete()
+    try:        
+        address = UserAppAddress.objects.get(id=item.user_app_address.id)        
         address.delete()
+        messages.add_message(request, messages.SUCCESS, "Excluído com sucesso!")
+    except Exception as e:
+        messages.add_message(request, messages.ERROR, "Ocorreu esse erro: {}".format(e))
+        
+    try:
+        item.delete()        
         messages.add_message(request, messages.SUCCESS, "Excluído com sucesso!")
     except Exception as e:
         messages.add_message(request, messages.ERROR, "Ocorreu esse erro: {}".format(e))
