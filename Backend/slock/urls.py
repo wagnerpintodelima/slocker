39f10ac7d3c915565e6e8b5d94d7c66f4bb85df6
Wagner <wagnerdelima2@gmail.com>
2026-09-10 11:05:12 -0300

    AJuste na url inicial

--- Backend/slock/urls.py
+++ Backend/slock/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from backend.Controller import AutomationController
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/gestao/login/', permanent=False), name='rootRedirect'),
     path('admin/', admin.site.urls),
     path('api/', include('backend.urlsAPI')),
     path('gestao/', include('backend.urls')),
